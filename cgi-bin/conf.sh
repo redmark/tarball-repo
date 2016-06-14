4eaa68e354e0c5e2f0c4047dca5d930c75583336
redmark <redmark@gmail.com>
2016-06-14 23:43:44 +0800

    remove test script

--- cgi-bin/conf.sh
+++
@@ -1,7 +0,0 @@
-##### common app param
-
-#directory of the entire app on the linux system, all bash cgi script must use this variable to locate resources on file system
-APP_DIR="$DOCUMENT_ROOT"
-
-#dir partial uri, all url must start with it
-APP_PATH=""
