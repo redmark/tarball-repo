c26204dea0ce7e4ab3b89084bd3beb2e6cb2f075
redmark <redmark@gmail.com>
2019-01-23 12:39:36 +0800

    fix openjdk.sh

--- cgi-bin/choose.d/openjdk.sh
+++ cgi-bin/choose.d/openjdk.sh
@@ -1,1 +1,1 @@
-tarball_name=$(ls $downloads_dir | egrep "^jdk-${request_tarball_version}.*-linux-x64\.tar\.gz$" | sort -r | head -1)
+tarball_name=$(ls $downloads_dir | egrep "openjdk-${request_tarball_version}.*_linux-x64_bin\.tar\.gz$" | sort -r | head -1)
